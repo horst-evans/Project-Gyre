be6706eac02f98a76466f4d47011a24b12ad1c75
horst-evans <32018144+horst-evans@users.noreply.github.com>
2020-11-08 23:56:43 -0600

    basic movement
    - added basic character controller script from documentation
    - switch capsule from capsule collider to mesh collider

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,39 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed = 2.0f;
     public float jumpHeight = 1.0f;
 
+    private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float gravityValue = -9.81f;
 
     private void Start()
     {
-        groundedPlayer = true;
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        Vector3 transverse = new Vector3();
-        float distance = playerSpeed * Time.deltaTime;
-        float sides = Input.GetAxis("Horizontal") * distance;
-        float forward = Input.GetAxis("Vertical") * distance;
-        float vertical = Mathf.Sqrt(Input.GetAxis("Jump") * jumpHeight * -3.0f * gravityValue);
-        transverse.x = sides;
-        transverse.y = vertical;
-        transverse.z = forward;
-        gameObject.transform.Translate(transverse, Space.Self);
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
