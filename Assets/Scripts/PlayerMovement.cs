08cb66b3a4ea0fd15295f6920ff544dcb15b9684
horst-evans <32018144+horst-evans@users.noreply.github.com>
2020-11-15 20:43:29 -0600

    Refine Player Movement
    Scene
    - changed base player movement
    - changed hori and verti movement values
    - changed out material on forward debug spur
    
    PlayerMovement
    - rotation of horizontal now on player, vertical only on camera
    - removed snap reset code
    - removed snap to forward code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,35 +22,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //mouse movement
-        transform.rotation = Quaternion.Euler(0, 0, 0);
         float h = horizontalSpeed * Input.GetAxis("Mouse X");
         float v = verticalSpeed * Input.GetAxis("Mouse Y");
         if (Input.GetMouseButton(0))
         {
             foreach(Camera cam in Camera.allCameras)
             {
-                cam.transform.Rotate(-v, h, 0);
+                cam.transform.Rotate(-v, 0, 0);
             }
-            //transform.Rotate(-v, h, 0);
+            transform.Rotate(0, h, 0);
         }
-
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
-
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         float angle = Camera.main.transform.rotation.eulerAngles.y;
         Vector3 rMove = Quaternion.AngleAxis(angle, Vector3.up) * move;
         controller.Move(rMove * Time.deltaTime * playerSpeed);
         //transform.rotation = Camera.main.transform.rotation;
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
         // Changes the height position of the player..
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
@@ -59,6 +50,5 @@ public class PlayerMovement : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
     }
 }
