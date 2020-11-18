4d58f39fe0d7f3369d0db73c9256ab9785739f2f
horst-evans <32018144+horst-evans@users.noreply.github.com>
2020-11-17 22:41:01 -0600

    water basics
    buoyancy: dictates how object floats in water
    wavemanager: dictates the generation and access to the sin wave
    watermanager: dictates how the plane is modified by the sin wave
    
    BUGS:
    the water manager and the wave manager are not in lockstep, need to debug why buoyancy and wave are grabbing different values at different locations.

---
+++ Assets/Scripts/Buoyancy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Buoyancy : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int buoyancyPoints = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngDrag = 0.5f;
+
+    private void FixedUpdate()
+    {
+        rb.AddForceAtPosition(Physics.gravity / buoyancyPoints, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+        //transform.position = new Vector3(transform.position.x, waveHeight, transform.position.z);
+        
+        if(transform.position.y < waveHeight)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        
+    }
+}
