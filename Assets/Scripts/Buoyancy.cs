55403dd3552a65ecf33ccc072a034e328427ecf4
horst-evans <32018144+horst-evans@users.noreply.github.com>
2020-12-14 18:54:10 -0600

    Fleet Buoyancy
    added buoyancy to default fleet ship
    TODO: buoyancy w/ player / other objects on top of the ship (mass increase?)

--- Assets/Scripts/Buoyancy.cs
+++ Assets/Scripts/Buoyancy.cs
@@ -8,17 +8,18 @@ public class Buoyancy : MonoBehaviour
     public int buoyancyPoints = 1; //how many instances of script affecting parent obj
     public float waterDrag = 0.99f;
     public float waterAngDrag = 0.5f;
+    public float gravityDrag = 1;
 
     private void FixedUpdate()
     {
-        rb.AddForceAtPosition(Physics.gravity / buoyancyPoints, transform.position, ForceMode.Acceleration);
+        rb.AddForceAtPosition(Physics.gravity * gravityDrag / buoyancyPoints, transform.position, ForceMode.Acceleration);
 
         float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x / WaveManager.instance.scale);
 
         if(transform.position.y < waveHeight)
         {
             float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
-            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier * gravityDrag, 0f), transform.position, ForceMode.Acceleration);
             rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
             rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
