bcb20a901a5cd73b23b77bc4264ca876d888a651
horst-evans <32018144+horst-evans@users.noreply.github.com>
2021-10-26 15:18:08 -0400

    Ship Controls
    - moved scripts to correct folder
    - created shipControls scripts
    - ship controls enabled on control switch
    - moved rigid body up to container ship and off of capsule
    - updated ship prefab

---
+++ Assets/Scripts/ShipControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    public float speed;
+    public float torque;
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb.AddForce(rb.transform.forward * speed);
+        }
+
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque(transform.up * torque * turn);
+    }
+}
